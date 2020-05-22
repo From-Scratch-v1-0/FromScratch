72f1d2ef623bcf28599a2f8c9a9106ae34f34bdd
GiorgiBezhanishvili <giorgi.bezhanishvili6@gmail.com>
2020-05-22 22:30:48 +0400

    Added : CatalogPage.cshtml;_CatalogLayout.cshtml; CatalogController/CatalogPage;

---
+++ FromScratch/Controllers/CatalogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FromScratch.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult CatalogPage()
+        {
+            return View();
+        }
+    }
+}
