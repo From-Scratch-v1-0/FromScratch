909e7f6a8f2505d9dd33a2d11c1bdf62df494ae0
Fascio98 <64546767+Fascio98@users.noreply.github.com>
2020-06-12 20:35:52 +0400

    გავაკეთე პროექტების რეფოზითორი! ვიმუშავე კატალოგკონტროლერში და შეძლებისდაგვარად გავაკეთე კატალოგისა და პროექტების ვიუ! ასევე გავაკეთე ცარიელი კატალოგის ვიუ, რომელიც ამ ეტაპზე შესაძლოა დაგვჭირდეს! გავტესტე პროექტის გვერდი და ფაქტია, რომ არ მუშაობს! Home გვერდზე მაბრუნებს! გაწეულმა სამუშაომ მაჩვენა, რომ კიდევ ბევრია გასაკეთებელი!

--- FromScratch/Controllers/CatalogController.cs
+++ FromScratch/Controllers/CatalogController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using FromScratch.Models.Repositories;
 using FS_DAL.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
+using Services.Repositories;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FromScratch.Controllers
 {
@@ -21,17 +25,23 @@ namespace FromScratch.Controllers
             _service = service;
             _userManager = userManager;
         }
-
         [AllowAnonymous]
-        public IActionResult CatalogPage(IEnumerable<ProjectProduct> project)
+        
+        [HttpGet]
+        public IActionResult CatalogPage(IEnumerable<ProjectProduct> prpr)
         {
-            return View();
+            //ორივე შემთხვევისათვის გავტესტე და მუშაობს! ჯერჯერობით იყოს ასე და თუ რამეა ჩაასწორეთ არ მაქვს პრობლემა!//
+            //დაახლოებით ასეთი რამე უნდა გაკეთდეს და თუ რამე შეცდომაა ან არ უნდა იყოს ისე, როგორც საჭიროა, მაშინ ჩაასწორეთ!//
+            //ეს ჯერჯერობით იყოს დაკომენტარებული//
+           /* if (prpr.Where(x=> x.ProjectKey>=1).Count()==0)
+                return View("EmptyCatalog");*/
+            return View(prpr);          
         }
-
-        public IActionResult Project(int projectkey, string projectname, int? projecttypekey, string description, int? rating) 
+        [HttpGet]
+        public IActionResult Project(Project proj) 
         {
-            return View();
+            return View(proj);
         }
-
+ 
     }
 }
